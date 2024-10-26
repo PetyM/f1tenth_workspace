f3b6affd066f3ae461a3a9c4459076351f069fb0
Michal Peterka <peterka.m@icloud.com>
2024-10-26 17:53:54 +0200

    fix world -> grid transformation

--- agent/agent/mapevaluator.py
+++ agent/agent/mapevaluator.py
@@ -1,3 +1,4 @@
+import scipy.spatial
 import rclpy
 import rclpy.node
 import rclpy.publisher
@@ -9,6 +10,7 @@ import rclpy.qos
 from tf2_ros import TransformStamped
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
+from geometry_msgs.msg import Transform
 
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 from geometry_msgs.msg import Vector3
@@ -36,10 +38,10 @@ class MapEvaluator(rclpy.node.Node):
         self.opponent_frame: str = f'{opponent_namespace}/{target_frame}'
         self.reference_frame: str = self.get_parameter('reference_frame').value
 
-        self.tf_buffer: Buffer = Buffer()
+        self.tf_buffer: Buffer = Buffer(rclpy.time.Duration(seconds=1), self)
         self.tf_listener: TransformListener = TransformListener(self.tf_buffer, self)
 
-        self.timer: rclpy.timer.Timer = self.create_timer(0.2, self.update)
+        self.timer: rclpy.timer.Timer = self.create_timer(0.01, self.update)
 
         qos_profile = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.RELIABLE,
                                            durability=rclpy.qos.DurabilityPolicy.TRANSIENT_LOCAL,
@@ -56,7 +58,7 @@ class MapEvaluator(rclpy.node.Node):
 
     def get_ego_position(self):
         try:
-            t = self.tf_buffer.lookup_transform(self.ego_frame, self.reference_frame, rclpy.time.Time())
+            t = self.tf_buffer.lookup_transform(self.reference_frame, self.ego_frame, rclpy.time.Time())
             return t.transform.translation
         except Exception as ex:
             self.get_logger().info(f'Could not transform {self.ego_frame} to {self.reference_frame}: {ex}')
@@ -65,7 +67,7 @@ class MapEvaluator(rclpy.node.Node):
 
     def get_opponent_position(self):
         try:
-            t = self.tf_buffer.lookup_transform(self.opponent_frame, self.reference_frame, rclpy.time.Time())
+            t = self.tf_buffer.lookup_transform(self.reference_frame, self.opponent_frame, rclpy.time.Time())
             return t.transform.translation
         except Exception as ex:
             self.get_logger().info(f'Could not transform {self.opponent_frame} to {self.reference_frame}: {ex}')
@@ -82,7 +84,7 @@ class MapEvaluator(rclpy.node.Node):
 
     def map_to_grid_coordinates(self, coordinate: Vector3) -> np.ndarray:
         row = ((coordinate.y - self.map_info.origin.position.y) / self.map_info.resolution)
-        column = ((-coordinate.x - self.map_info.origin.position.x) / self.map_info.resolution)
+        column = ((coordinate.x - self.map_info.origin.position.x) / self.map_info.resolution)
         return np.array([row, column], dtype=int)
 
 
@@ -94,19 +96,14 @@ class MapEvaluator(rclpy.node.Node):
         opponent_position = self.get_opponent_position()
         if not ego_position or not opponent_position:
             return
-         
 
         ego_grid_position = self.map_to_grid_coordinates(ego_position)
         opponent_grid_position = self.map_to_grid_coordinates(opponent_position)
-        self.get_logger().info(f'Ego position {ego_position}, opponent position {opponent_position}') 
-        self.get_logger().info(f'Ego position grid {ego_grid_position}, opponent position grid {opponent_grid_position}')
 
         self.costmap = self.map.copy()
 
-        self.costmap[0:30, 0:30] = -100
-        self.costmap[-30:-1, -30:-1] = -100
-
         self.costmap[opponent_grid_position[0] - 10 : opponent_grid_position[0] + 10, opponent_grid_position[1] - 10 : opponent_grid_position[1] + 10] = -100
+        self.costmap[ego_grid_position[0] - 10 : ego_grid_position[0] + 10, ego_grid_position[1] - 10 : ego_grid_position[1] + 10] = 100
 
         costmap = OccupancyGrid()
         costmap.data = self.costmap.reshape((-1)).tobytes()
@@ -115,7 +112,7 @@ class MapEvaluator(rclpy.node.Node):
         costmap.info = self.map_info
         self.costmap_publisher.publish(costmap)
 
-        # self.get_logger().info(f"(update) took {(self.get_clock().now() - start).nanoseconds / 1e6} ms")
+        self.get_logger().info(f"(update) took {(self.get_clock().now() - start).nanoseconds / 1e6} ms")
 
 
 def main():
