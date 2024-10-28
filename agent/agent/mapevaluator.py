1c299ff690e54f4516cedbb66d1a91ca55a25a33
Michal Peterka <peterka.m@icloud.com>
2024-10-28 16:42:24 +0100

    add costs for ego and opponent positions

--- agent/agent/mapevaluator.py
+++ agent/agent/mapevaluator.py
@@ -16,6 +16,7 @@ from nav_msgs.msg import OccupancyGrid, MapMetaData
 from geometry_msgs.msg import Vector3
 
 import numpy as np
+import scipy.ndimage
 
 class MapEvaluator(rclpy.node.Node):
     def __init__(self):
@@ -54,7 +55,21 @@ class MapEvaluator(rclpy.node.Node):
 
         self.costmap_publisher: rclpy.publisher.Publisher = self.create_publisher(OccupancyGrid, costmap_topic, qos_profile)
         self.costmap: np.ndarray = None
+        self.car_size: float = 0.4
 
+        self.evaluation_area_size: int = 199
+        self.mask_offset: int = int((self.evaluation_area_size - 1) // 2)
+
+
+        opponent_mask = np.zeros((self.evaluation_area_size, self.evaluation_area_size))
+        opponent_mask[self.mask_offset - 4 : self.mask_offset + 4, self.mask_offset - 4 : self.mask_offset + 4] = 1
+        opponent_mask = scipy.ndimage.gaussian_filter(opponent_mask, sigma=20)
+        self.opponent_mask: np.ndarray = 100 * (opponent_mask / opponent_mask.max())
+
+        ego_mask: np.ndarray = np.zeros((self.evaluation_area_size, self.evaluation_area_size))
+        ego_mask[self.mask_offset - 4 : self.mask_offset + 4, self.mask_offset - 4 : self.mask_offset + 4] = 1
+        ego_mask = scipy.ndimage.gaussian_filter(ego_mask, sigma=20)
+        self.ego_mask: np.ndarray = -100 * (ego_mask / ego_mask.max())
 
     def get_ego_position(self):
         try:
@@ -76,10 +91,24 @@ class MapEvaluator(rclpy.node.Node):
 
     def update_map(self, map: OccupancyGrid):
         self.get_logger().error('Map updated')
-        self.map = np.reshape(map.data, (map.info.height, map.info.width))
         self.map_info = map.info
         self.get_logger().warn(f"{map.info.origin.position.x}, {map.info.origin.position.y}, {map.info.origin.position.z}")
         self.get_logger().warn(f"{map.info.origin.orientation.x}, {map.info.origin.orientation.y}, {map.info.origin.orientation.z}, {map.info.origin.orientation.w}")
+
+        costmap = np.reshape(map.data, (map.info.height, map.info.width))
+
+        dilation_diameter = int(np.ceil((1.2 * self.car_size) / self.map_info.resolution))
+        if dilation_diameter % 2 == 0:
+            dilation_diameter += 1
+        dilation_footprint_radius = np.ceil(dilation_diameter / 2)
+
+        dilation_footprint = np.zeros((dilation_diameter, dilation_diameter))
+        rx, ry = np.indices(dilation_footprint.shape)
+        radius_grid = (rx - dilation_footprint_radius)**2 + (ry - dilation_footprint_radius)**2
+        dilation_footprint[radius_grid <= dilation_footprint_radius**2] = 1
+
+        self.map = scipy.ndimage.morphology.grey_dilation(costmap, footprint=dilation_footprint)
+
 
 
     def map_to_grid_coordinates(self, coordinate: Vector3) -> np.ndarray:
@@ -100,10 +129,16 @@ class MapEvaluator(rclpy.node.Node):
         ego_grid_position = self.map_to_grid_coordinates(ego_position)
         opponent_grid_position = self.map_to_grid_coordinates(opponent_position)
 
-        self.costmap = self.map.copy()
+        ego_costmap = np.zeros_like(self.map)
+        ego_costmap[ego_grid_position[0] - self.mask_offset - 1 : ego_grid_position[0] + self.mask_offset, ego_grid_position[1] - self.mask_offset - 1 : ego_grid_position[1] + self.mask_offset] = self.ego_mask
 
-        self.costmap[opponent_grid_position[0] - 10 : opponent_grid_position[0] + 10, opponent_grid_position[1] - 10 : opponent_grid_position[1] + 10] = -100
-        self.costmap[ego_grid_position[0] - 10 : ego_grid_position[0] + 10, ego_grid_position[1] - 10 : ego_grid_position[1] + 10] = 100
+        opponent_costmap = np.zeros_like(self.map)
+        opponent_costmap[opponent_grid_position[0] - self.mask_offset - 1: opponent_grid_position[0] + self.mask_offset, opponent_grid_position[1] - self.mask_offset -1 : opponent_grid_position[1] + self.mask_offset] = self.opponent_mask
+
+        costmap = np.add(ego_costmap, opponent_costmap)
+        costmap[self.map > 0] = 100
+
+        self.costmap = costmap
 
         costmap = OccupancyGrid()
         costmap.data = self.costmap.reshape((-1)).tobytes()
