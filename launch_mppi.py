46bf4526a2739253e0a8d1f964c2faaac8ba2ced
Michal Peterka <peterka.m@icloud.com>
2024-12-29 16:55:20 +0100

    add mppi controller node to nav2 lifecycle manager, use example parameters for mppi controller

--- launch_mppi.py
+++ launch_mppi.py
@@ -46,7 +46,7 @@ def generate_launch_description():
         output='screen',
         parameters=[{'use_sim_time': True},
                     {'autostart': True},
-                    {'node_names': ['map_server']}],
+                    {'node_names': ['map_server', 'ego_agent_controller']}],
         arguments=["--ros-args", "--log-level", "warn"]
     )
 
@@ -63,14 +63,100 @@ def generate_launch_description():
         package='nav2_controller',
         executable='controller_server',
         name='ego_agent_controller',
-        parameters=[{'planner_plugin': "nav2_mppi_controller/MPPIController"},
-                    {'max_vel_x': 0.5},
-                    {'max_vel_y': 0.0},
-                    {'max_vel_theta': 1.0},
-                    {'min_vel_x': 0.0},
-                    {'min_vel_theta': -1.0},
-                    {'acc_lim_x': 0.5},
-                    {'acc_lim_theta': 0.5}]
+        parameters=[{
+                'controller_frequency': 30.0,
+                'FollowPath': {
+                    'plugin': 'nav2_mppi_controller::MPPIController',
+                    'time_steps': 56,
+                    'model_dt': 0.05,
+                    'batch_size': 2000,
+                    'vx_std': 0.2,
+                    'vy_std': 0.2,
+                    'wz_std': 0.4,
+                    'vx_max': 0.5,
+                    'vx_min': -0.35,
+                    'vy_max': 0.5,
+                    'wz_max': 1.9,
+                    'iteration_count': 1,
+                    'prune_distance': 1.7,
+                    'transform_tolerance': 0.1,
+                    'temperature': 0.3,
+                    'gamma': 0.015,
+                    'motion_model': 'DiffDrive',
+                    'visualize': False,
+                    'TrajectoryVisualizer': {
+                        'trajectory_step': 5,
+                        'time_step': 3
+                    },
+                    'AckermannConstraints': {
+                        'min_turning_r': 0.2
+                    },
+                    'critics': [
+                        "ConstraintCritic", "CostCritic", "GoalCritic", 
+                        "GoalAngleCritic", "PathAlignCritic", 
+                        "PathFollowCritic", "PathAngleCritic", "PreferForwardCritic"
+                    ],
+                    'ConstraintCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 4.0
+                    },
+                    'GoalCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 5.0,
+                        'threshold_to_consider': 1.4
+                    },
+                    'GoalAngleCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 3.0,
+                        'threshold_to_consider': 0.5
+                    },
+                    'PreferForwardCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 5.0,
+                        'threshold_to_consider': 0.5
+                    },
+                    'CostCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 3.81,
+                        'critical_cost': 300.0,
+                        'consider_footprint': True,
+                        'collision_cost': 1000000.0,
+                        'near_goal_distance': 1.0,
+                        'trajectory_point_step': 2
+                    },
+                    'PathAlignCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 14.0,
+                        'max_path_occupancy_ratio': 0.05,
+                        'trajectory_point_step': 4,
+                        'threshold_to_consider': 0.5,
+                        'offset_from_furthest': 20,
+                        'use_path_orientations': False
+                    },
+                    'PathFollowCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 5.0,
+                        'offset_from_furthest': 5,
+                        'threshold_to_consider': 1.4
+                    },
+                    'PathAngleCritic': {
+                        'enabled': True,
+                        'cost_power': 1,
+                        'cost_weight': 2.0,
+                        'offset_from_furthest': 4,
+                        'threshold_to_consider': 0.5,
+                        'max_angle_to_furthest': 1.0,
+                        'forward_preference': True
+                    }
+                }
+            }]
     )
 
     ego_agent_node = Node(
