6e6039e7f2dfaa1eab87fe653f8aa07efd098869
Michal Peterka <peterka.m@icloud.com>
2025-01-27 22:19:26 +0100

    AgentBase implementation in cpp

--- cppagent/include/cppagent/agentbase.h
+++ cppagent/include/cppagent/agentbase.h
@@ -1,12 +1,59 @@
 #ifndef AGENTBASE_H
 #define AGENTBASE_H
 
+#include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+
 #include "rclcpp/rclcpp.hpp"
 
-class AgentBase
+#include <array>
+
+class AgentBase : public rclcpp::Node
 {
+    using BaseClass = rclcpp::Node;
 public:
     AgentBase();
+
+protected:
+    using State = std::vector<double>;
+    const State& state() const;
+    const State& opponentState() const;
+
+    virtual void updateControl();
+    
+    struct Action
+    {
+        double velocity;
+        double steeringAngle;
+    };
+    virtual Action plan() = 0;
+
+    const std::string& agentNamespace() const;
+    const std::string& opponentNamespace() const;
+    const std::string& stateTopic() const;
+    const std::string& driveTopic() const;
+    double velocityGain() const;
+    bool opponentPresent() const;
+
+private:
+    using AckermannDriveStamped = ackermann_msgs::msg::AckermannDriveStamped;
+    rclcpp::Publisher<AckermannDriveStamped>::SharedPtr m_drivePublisher;
+
+    using Float64MultiArray = std_msgs::msg::Float64MultiArray;
+    rclcpp::Subscription<Float64MultiArray>::SharedPtr m_stateSubscription;
+    rclcpp::Subscription<Float64MultiArray>::SharedPtr m_opponentStateSubscription;
+
+    rclcpp::TimerBase::SharedPtr m_timer;
+
+    State m_state;
+    State m_opponentState;
+
+    std::string m_agentNamespace;
+    std::string m_opponentNamespace;
+    std::string m_stateTopic;
+    std::string m_driveTopic;
+    double m_velocityGain;
+    bool m_opponentPresent;
 };
 
 #endif // AGENTBASE_H
--- cppagent/src/agentbase.cpp
+++ cppagent/src/agentbase.cpp
@@ -1,14 +1,100 @@
 #include "cppagent/agentbase.h"
 
-AgentBase::AgentBase()
-{
+#include <format>
 
+
+AgentBase::AgentBase()
+    : BaseClass("agent")
+{
+    declare_parameter("agent_namespace", "ego_racecar");
+    m_agentNamespace = get_parameter("agent_namespace").as_string();
+    declare_parameter("opponent_namespace", "opp_racecar");
+    m_opponentNamespace = get_parameter("opponent_namespace").as_string();
+    declare_parameter("state_topic", "state");
+    m_stateTopic = get_parameter("state_topic").as_string();
+    declare_parameter("drive_topic", "drive");
+    m_driveTopic = get_parameter("drive_topic").as_string();
+    declare_parameter("velocity_gain", 1.0);
+    m_velocityGain = get_parameter("velocity_gain").as_double();
+    declare_parameter("opponent_present", false);
+    m_opponentPresent = get_parameter("opponent_present").as_bool();
+
+    const std::string driveTopic = std::format("{}/{}", m_agentNamespace, m_driveTopic);
+    m_drivePublisher = create_publisher<AckermannDriveStamped>(driveTopic, 1);
+
+    const std::string stateTopic = std::format("{}/{}", m_agentNamespace, m_stateTopic);
+    const auto stateCallback = [this](Float64MultiArray::SharedPtr state)
+    {
+        m_state = state->data;
+    };
+    m_stateSubscription = create_subscription<Float64MultiArray>(stateTopic, 1, stateCallback);
+
+    const std::string opponentStateTopic = std::format("{}/{}", m_opponentNamespace, m_stateTopic);
+    const auto opponentStateCallback = [this](Float64MultiArray::SharedPtr state)
+    {
+        m_opponentState = state->data;
+    };
+    m_opponentStateSubscription = create_subscription<Float64MultiArray>(opponentStateTopic, 1, opponentStateCallback);
+
+    m_timer = create_wall_timer(std::chrono::seconds(1), [this](){ updateControl(); });
 }
+
+const AgentBase::State& AgentBase::state() const
+{
+    return m_state;
+}
+
+const AgentBase::State& AgentBase::opponentState() const
+{
+    return m_opponentState;
+}
+
+void AgentBase::updateControl()
+{
+    auto action = plan();
+
+    auto message = AckermannDriveStamped();
+    message.header.stamp = get_clock()->now();
+    message.drive.speed = action.velocity;
+    message.drive.steering_angle = action.steeringAngle;
+    m_drivePublisher->publish(message);
+}
+
+const std::string& AgentBase::agentNamespace() const
+{
+    return m_agentNamespace;
+}
+
+const std::string& AgentBase::opponentNamespace() const
+{
+    return m_opponentNamespace;
+}
+
+const std::string& AgentBase::stateTopic() const
+{
+    return m_stateTopic;
+}
+
+const std::string& AgentBase::driveTopic() const
+{
+    return m_driveTopic;
+}
+
+double AgentBase::velocityGain() const
+{
+    return m_velocityGain;
+}
+
+bool AgentBase::opponentPresent() const
+{
+    return m_opponentPresent;
+}
+
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("minimal_client");
+//   rclcpp::spin(std::make_shared<AgentBase>());
   rclcpp::shutdown();
   return 0;
 }
